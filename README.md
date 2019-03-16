9bfc06d38a8eb6087fe37963dfa7cbe354bdb114
ian <ian.miell@gmail.com>
2019-03-16 06:54:27 +0000

    latest

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # introduction-to-bash
+
+`exercises` - course exercises
+
+`code`      - the example code in scripts
+
+`slides`    - asciidoc slides
--- code/1.2.1.globs.sh
+++ code/1.2.1.globs.sh
@@ -2,10 +2,21 @@
 mkdir lbthw_glob
 cd lbthw_glob
 touch file1 file2 file3 file10 file11
+# *
 ls *
 echo *
 ls '*'
 ls "*"
+# ?
+ls file[?]
+ls file[*]
+# [n]
+ls file[1]
+# [n-m]
+ls file[1-3]
+ls f*[1-3]1
+man ascii
+# dotfiles
 touch .adotfile
 mkdir .adotdir
 ls *
@@ -16,6 +27,9 @@ echo 'Run .*'
 echo .*
 echo 'Run .a*'
 echo .a*
+ls .
+ls ..
+# TODO: vs regexp
 # Cleanup
 cd ..
 rm -rf lbthw_glob
